27b3f2c3898b89308fc16d4d6e71e783124cb81d
Xibo Wang <18601271489@163.com>
2023-01-14 14:27:01 +0000

    test push

--- IoTSimulator/simulator.py
+++ IoTSimulator/simulator.py
@@ -1,9 +1,26 @@
+import math
+import random
+import time
+
+
 class Simulator:
-    def start() -> None:
+    def __init__(self):
+        self.x = 0
+
+    def start(self) -> None:
         pass
 
-    def generate_data():
+    def generate_data(self):
+        self.x += 1
+        value = 2 * math.sin(self.x) + 3
+        value += 3 * random.random()
+        return value
+
+    def send_data(self):
         pass
 
-    def send_data():
-        pass
+if __name__ == '__main__':
+    s = Simulator()
+    while(True):
+        time.sleep(1)
+        print(s.x, s.generate_data())
