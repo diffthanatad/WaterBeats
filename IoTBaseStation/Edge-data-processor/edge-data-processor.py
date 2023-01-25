8992c346a21133f798a2fe499f89027bff7b4f7c
Zenonist <phonarnun@hotmail.com>
2023-01-25 14:50:55 +0000

    feat: add min,max function and handle http error

--- IoTBaseStation/Edge-data-processor/edge-data-processor.py
+++ IoTBaseStation/Edge-data-processor/edge-data-processor.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
 import redis
 import numpy as np
+from aiohttp import web
+from werkzeug.exceptions import HTTPException
 
 webapp = Flask(__name__)
 
-class web_application:
+class edge_data_processor:
     # * This class is used to create a web application that will receive the data from the sensor
     
     def __init__(self, url: str, port: int, debug: bool = False):
@@ -12,34 +14,50 @@ class web_application:
         self.port = port
         self.debug = debug
 
-    # TODO : fix the https status code 403 and add more url if needed
     @webapp.route('/new_data', methods=['POST'])
     def __receivesensor():
+        # !: Databuffer component is not implemented yet
         data = request.json
         print(data)
         return jsonify(data)
 
+    # * Handle the error
+    @webapp.errorhandler(code_or_exception=HTTPException)
+    def handle_exception(self,e):
+        response = e.get_response()
+        response.data = json.dumps({
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        })
+        response.content_type = "application/json"
+        return response
+
+
     def runserver(self):
         webapp.run(host=self.url, port=self.port, debug=self.debug)
+        # TODO: Implement the code to send data back to main machine or other iot base station
+        """
+            pseudocode:
+            while True:
+                if sleep or timeout is finised:
+                    if data in Redis is not empty: (If the existing dat is not sent):
+                        send data to main machine or other iot base station (if it could not reached the main machine) -> using aiohttp to send http request 
+                    sleep or timeout
+        """
         
-
-class redis_database:
-    def __init__(self, host: str, port: int, db: int):
-        self.host = host
-        self.port = port
-        self.db = db
-        self.pool = None
-        self.redisdb = None
-        
-    def connect(self):
-        self.pool = redis.ConnectionPool(host=self.host, port=self.port, db=self.db)
-        self.redisdb = redis.Redis(connection_pool=self.pool)
 
 class processing_data:
     # * This class is used to process the data from the sensor (how to process the data is not decided yet)
-    
     def compute_avg(self,data: list()) -> float:
         return np.mean(data)
 
-web_app = web_application("localhost", 5000, True)
+    def compute_min(self,data: list()) -> float:
+        return np.min(data)
+    
+    def compute_max(self,data: list()) -> float:
+        return np.max(data)
+
+
+web_app = web_application("localhost", 5500, True)
 web_app.runserver()
