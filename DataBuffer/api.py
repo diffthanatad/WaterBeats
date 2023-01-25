f7e128638b3b8eba17bb02e180a734e03004f016
Sean Chao <seanchao0804@gmail.com>
2023-01-25 15:32:53 +0000

    feat: data buffer for sensors

---
+++ DataBuffer/api.py
@@ -0,0 +1,81 @@
+import json
+from flask import Flask, make_response, request
+import redis
+from data_buffer import DataBuffer
+
+app = Flask(__name__)
+
+
+data_buffer = DataBuffer()
+
+
+def get_sensor_data(data_buffer: DataBuffer, sensor_id, timestamp):
+    key = f"{sensor_id}:{timestamp}"
+    fvs = data_buffer.hgetall(key)
+    if not fvs:
+        return None
+    return {
+        "sensorId": sensor_id,
+        "timestamp": timestamp,
+        "sensorType": str(fvs[b"sensorType"], encoding="utf-8"),
+        "data": float(fvs[b"data"]),
+    }
+
+
+def set_sensor_data(db: DataBuffer, data):
+    key = f"{data['sensorId']}:{data['timestamp']}"
+    fv_pairs = {
+        "sensorType": data["sensorType"],
+        "data": data["data"],
+    }
+    db.hset(key, fv_pairs)
+
+
+def delete_sensor_data(db: DataBuffer, sensor_id, timestamp):
+    key = f"{sensor_id}:{timestamp}"
+    return db.delete(key)
+
+
+@app.errorhandler(redis.exceptions.ConnectionError)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    print(e)
+    response = make_response()
+    response.data = json.dumps(
+        {
+            "erroNo": 4,
+            "message": "Redis connection error",
+            "data": None,
+        }
+    )
+    response.status_code = 500
+    response.content_type = "application/json"
+    return response
+
+
+@app.route("/sensor", methods=["GET", "POST", "DELETE"])
+def sensor_api():
+    if request.method == "GET":
+        sensor_id = request.args.get("sensor_id")
+        timestamp = request.args.get("timestamp")
+        data = get_sensor_data(data_buffer, sensor_id, timestamp)
+        if data is None:
+            return ("", 204)
+        resp_body = {"erroNo": 0, "message": "OK", "data": data}
+        return (resp_body, 200)
+    elif request.method == "POST":
+        data = request.json
+        set_sensor_data(data_buffer, data)
+        return ("", 201)
+    elif request.method == "DELETE":
+        sensor_id = request.args.get("sensor_id")
+        timestamp = request.args.get("timestamp")
+        nums_del = delete_sensor_data(data_buffer, sensor_id, timestamp)
+        if nums_del == 0:
+            return ("", 204)
+        return ("", 202)
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=23333, debug=True)
--- DataBuffer/data_buffer.py
+++ DataBuffer/data_buffer.py
@@ -1,3 +1,4 @@
+from typing import List
 import redis
 
 
@@ -11,10 +12,29 @@ class DataBuffer:
     def get(self, key):
         return self._get_redis().get(key)
 
-    def set(self, key, value):
-        self._get_redis().set(key, value)
+    def hgetall(self, key):
+        return self._get_redis().hgetall(key)
 
-    def _get_redis(self):
+    def pipeline_hget(self, name, fields: List[str]):
+        pipeline = self._get_redis().pipeline()
+        for field in fields:
+            pipeline.hget(name, field)
+        return pipeline.execute()
+
+    def hset(self, name, field_and_values: dict):
+        pipeline = self._get_redis().pipeline()
+        for field, value in field_and_values.items():
+            pipeline.hset(name, field, value)
+        pipeline.execute()
+
+    def delete(self, key):
+        num_keys_del = self._get_redis().delete(key)
+        return num_keys_del
+
+    def _get_redis(self) -> redis.Redis:
         if self._redis is None:
-            self._redis = redis.Redis(host=self._host, port=self._port, db=self._db)
+            try:
+                self._redis = redis.Redis(host=self._host, port=self._port, db=self._db)
+            except redis.exceptions.ConnectionError as e:
+                print("Redis connection error", e)
         return self._redis
