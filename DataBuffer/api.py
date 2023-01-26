f7b003200374ab23ecb6b5c3215546ccdff77f1f
Sean Chao <seanchao0804@gmail.com>
2023-01-26 12:28:58 +0000

    feat: DataBuffer for Actuators

--- DataBuffer/api.py
+++ DataBuffer/api.py
@@ -35,6 +35,31 @@ def delete_sensor_data(db: DataBuffer, sensor_id, timestamp):
     key = f"{sensor_id}:{timestamp}"
     return db.delete(key)
 
+def get_instruction_data(data_buffer: DataBuffer, actuator_id, timestamp):
+    key = f"{actuator_id}:{timestamp}"
+    fvs = data_buffer.hgetall(key)
+    if not fvs:
+        return None
+    return {
+        "actuatorId": actuator_id,
+        "timestamp": timestamp,
+        "actuatorType": str(fvs[b"actuatorType"], encoding="utf-8"),
+        "data": float(fvs[b"data"]),
+    }
+
+
+def set_actuator_data(db: DataBuffer, data):
+    key = f"{data['actuatorId']}:{data['timestamp']}"
+    fv_pairs = {
+        "sensorType": data["actuatorType"],
+        "data": data["data"],
+    }
+    db.hset(key, fv_pairs)
+
+
+def delete_actuator_data(db: DataBuffer, actuator_id, timestamp):
+    key = f"{actuator_id}:{timestamp}"
+    return db.delete(key)
 
 @app.errorhandler(redis.exceptions.ConnectionError)
 def handle_exception(e):
@@ -72,6 +97,28 @@ def sensor_api():
         sensor_id = request.args.get("sensor_id")
         timestamp = request.args.get("timestamp")
         nums_del = delete_sensor_data(data_buffer, sensor_id, timestamp)
+        if nums_del == 0:
+            return ("", 204)
+        return ("", 202)
+
+@app.route("/instruction", methods=["GET", "POST", "DELETE"])
+def actuator_api():
+    if request.method == "GET":
+        actuator_id = request.args.get("actuator_id")
+        timestamp = request.args.get("timestamp")
+        data = get_instruction_data(data_buffer, actuator_id, timestamp)
+        if data is None:
+            return ("", 204)
+        resp_body = {"erroNo": 0, "message": "OK", "data": data}
+        return (resp_body, 200)
+    elif request.method == "POST":
+        data = request.json
+        set_actuator_data(data_buffer, data)
+        return ("", 201)
+    elif request.method == "DELETE":
+        actuator_id = request.args.get("actuator_id")
+        timestamp = request.args.get("timestamp")
+        nums_del = delete_actuator_data(data_buffer, actuator_id, timestamp)
         if nums_del == 0:
             return ("", 204)
         return ("", 202)
