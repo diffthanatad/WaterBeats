7792d349fe83cda822fe741e3b2559e741034280
Xibo Wang <18601271489@163.com>
2023-02-02 21:18:41 +0000

    Dockerfile

--- IoTActuator/server/serverTest.py
+++ IoTActuator/server/serverTest.py
@@ -1,4 +1,6 @@
 import asyncio
+import time
+
 import websockets
 import json
 
@@ -6,12 +8,15 @@ async def handle_message(websocket, path):
     while True:
         message = await websocket.recv()
         print(message)
-        command = input("命令：")
+        time.sleep(2)
+        command = "on"
         # print(f"Received message: {message}")
         data = {"instruction" : command}
         await websocket.send(json.dumps(data))
 
-start_server = websockets.serve(handle_message, "localhost", 8765)
+start_server = websockets.serve(handle_message, "0.0.0.0", 8765)
+# start_server = websockets.serve(handle_message, "localhost", 8765)
+
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
