500ad111470f97d1dbb1bd801d19085bbfaabcde
Zenonist <phonarnun@hotmail.com>
2023-02-06 16:38:16 +0000

    Upload the incomplete skeleton code

---
+++ DataManagement/data_management.py
@@ -0,0 +1,58 @@
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+class DataManagement:
+    
+    def __init__(self, bucket: str, org: str, token: str, url: str):
+        self.bucket = bucket
+        self.org = org
+        self.token = token
+        self.url = url
+        self.client = influxdb_client.InfluxDBClient(url=self.url, token=self.token, org=self.org)
+        self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
+        
+    async def write_data(self, data_point: influxdb_client.Point) -> str:
+        """write data to the database
+
+        Args:
+            data_point (influxdb_client.Point): Data point
+
+        Returns:
+            str: status
+        """
+        print(f'{self.org} {self.bucket} {self.token} {self.url} {self.bucket}')
+        try :
+            await self.write_api.write(self.bucket, self.org, data_point)
+            return f"Data inserted"
+        except Exception as e:
+            return f"Error while inserting data to the Database: {e}"
+        
+def insert_sensor_data(DM: DataManagement,sensor_id: str, sensor_type: str, data: float, unit: str, longitude: float, latitude: float, timestamp: str) -> str:
+    """insert data to the data buffer
+    Args:
+        sensor_id (str): Sensor ID
+        sensor_type (str): Sensor type
+        data (float): Data
+        unit (str): Unit of the data
+        longitude (float): Longitude of the sensor
+        latitude (float): Latitude of the sensor
+        timestamp (str): Timestamp of the data
+    Raises:
+        Exception: Error while inserting data to the DataBuffer
+    Returns:
+        str: Data inserted to DataBuffer
+    """
+    
+    # * Create data point
+    data_point = influxdb_client.Point("sensor_data") \
+        .tag("sensor_id", sensor_id) \
+        .tag("sensor_type", sensor_type) \
+        .field("data", data) \
+        .field("unit", unit) \
+        .field("longitude", longitude) \
+        .field("latitude", latitude) \
+        .time(timestamp)
+    
+    # * Write data to the database
+    return DM.write_data(data_point)
+
---
+++ DataManagement/test_data_management.py
@@ -0,0 +1,21 @@
+from data_management import DataManagement, insert_sensor_data
+from create_token import find_waterbeats_buckets, create_token_for_bucket
+import pytest
+
+WB_bucket = find_waterbeats_buckets()
+WB_token = create_token_for_bucket(WB_bucket["id"],WB_bucket["orgID"])
+WB_datamanagement = DataManagement(WB_bucket, WB_bucket["orgID"], WB_token, "https://localhost")
+
+
+
+@pytest.mark.asyncio
+async def test_insert_sensor_data():
+    sensor_id = "test_sensor"
+    sensor_type = "test_type"
+    sensor_data = 50
+    unit = "test_unit"
+    longitude = 0
+    latitude = 0
+    timestamp = "2021-02-22T15:00:00Z"
+    response = await insert_sensor_data(WB_datamanagement, sensor_id, sensor_type, sensor_data, unit, longitude, latitude, timestamp)
+    assert response == "Data inserted"
