f0a7425a92304bbb9f65755265fd16e45c49313d
Zenonist <phonarnun@hotmail.com>
2023-02-09 10:36:29 +0000

    test: add test cases

--- DataManagement/test_data_management.py
+++ DataManagement/test_data_management.py
@@ -10,7 +10,7 @@ WB_token = create_token_for_bucket(WB_bucket["id"],WB_bucket["orgID"])
 WB_datamanagement = DataManagement(WB_bucket["id"], WB_bucket["orgID"], WB_token, "http://localhost:8086")
 
 def test_insert_sensor_data():
-    sensor_id = "test_sensor"
+    sensor_id = "1"
     sensor_type = "test_type"
     sensor_data = random.uniform(-10, 1000)
     unit = "test_unit"
@@ -23,7 +23,7 @@ def test_insert_sensor_data():
 @pytest.mark.asyncio
 async def test_insert_sensor_data_api():
     json_data = {
-        "sensor_id": "1",
+        "sensor_id": "2",
         "sensor_type": "temperature",
         "data": 26.4,
         "unit": "C",
@@ -34,3 +34,36 @@ async def test_insert_sensor_data_api():
     async with aiohttp.ClientSession() as session:
         async with session.post('http://localhost:5555/sensor_data', json=json_data) as resp:
             assert resp.status == 200
+            
+@pytest.mark.asyncio
+async def test_insert_sensor_data_api_missing_data():
+    json_data = {
+        "sensor_id": "3",
+        "sensor_type": "temperature",
+        "data": 26.4,
+        "unit": "C",
+        "longitude": 2134.1234124,
+        "latitude": 321444.12341234
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post('http://localhost:5555/sensor_data', json=json_data) as resp:
+            assert resp.status == 400
+            
+@pytest.mark.asyncio
+async def test_insert_sensor_data_api_2_interval():
+    json_data = {
+        "sensor_id": str(random.randint(5,100)),
+        "sensor_type": "temperature",
+        "data": random.uniform(-10, 1000),
+        "unit": "C",
+        "longitude": random.uniform(-1000, 1000),
+        "latitude": random.uniform(-1000, 1000),
+        "timestamp": time.time_ns()
+    }
+    for i in range(10):
+        async with aiohttp.ClientSession() as session:
+            async with session.post('http://localhost:5555/sensor_data', json=json_data) as resp:
+                print(resp.text)
+                assert resp.status == 200
+        print(i)
+        time.sleep(2)
