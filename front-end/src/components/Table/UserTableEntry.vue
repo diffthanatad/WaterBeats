a5ee24c52479737d3bbca20566d17767a79c9a90
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-02-24 22:15:28 +0000

    :construction: Working on the admin page.

---
+++ front-end/src/components/Table/UserTableEntry.vue
@@ -0,0 +1,50 @@
+<template>
+    <tr>
+        <td>{{ item.name }}</td>
+        <td>{{ item.username }}</td>
+        <td>{{ item.role }}</td>
+        <td>{{ item.status ? "Active" : "Deactivated" }}</td>
+        <td>
+            <button type="button" class="btn btn-warning" style="margin-right: 10px;" @click.prevent="">
+                <i class="bi bi-pencil-square"></i>
+            </button>
+            <button type="button" class="btn btn-danger" @click.prevent="">
+                <i class="bi bi-trash"></i>
+            </button>
+        </td>
+    </tr>
+</template>
+  
+<script>
+
+export default {
+    name: "UserTableEntry",
+    props: {
+        item: Object,
+    },
+    components: {
+    },
+    methods: {
+        //   async cancel() {
+        //     try {
+        //       const ok = await this.$refs.confirmDialogue.show({
+        //         title: 'แก้ไขสถานะคำสั่งซื้อ',
+        //         message: 'คุณต้องการจะยกเลิกสินค้าใช่ไหม?',
+        //         okButton: 'ยืนยัน',
+        //       });
+
+        //       if (ok && !this.orderItem.cancel) {
+        //         await cancelOrder(this.orderItem.id)
+        //         this.$emit('clicked', "")
+        //       } else {
+        //         // nothing.
+        //       }
+        //     } catch (error) {
+        //       console.log("error:", error);
+        //     }
+        //   }
+    }
+}
+</script>
+  
+<style scoped></style>
--- front-end/src/views/AdminView.vue
+++ front-end/src/views/AdminView.vue
@@ -1,14 +1,65 @@
 <template>
-  <div class="admin">
+  <div class="admin container">
     <h1>Admin</h1>
+    <table id="orderDetail" class="table table-hover table-sm">
+      <thead>
+        <tr>
+          <th style="width:35%;">Fullname</th>
+          <th style="width:20%;">Username</th>
+          <th style="width:15%;">Role</th>
+          <th style="width:15%;">Status</th>
+          <th style="width:15%;"></th>
+        </tr>
+      </thead>
+      <transition-group name="list-user" tag="tbody">
+        <UserTableEntry v-for="user in users" :item="user" :key="user.id"></UserTableEntry>
+      </transition-group>
+    </table>
   </div>
 </template>
 
 <script>
+import UserTableEntry from '@/components/Table/UserTableEntry.vue';
 
 export default {
   name: 'AdminView',
   components: {
-  }
+    UserTableEntry,
+  },
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Adam Smith",
+          username: "adam01",
+          role: "admin",
+          status: 1
+        },
+        {
+          id: 2,
+          name: "David Brown",
+          username: "david01",
+          role: "user",
+          status: 0
+        },
+        {
+          id: 3,
+          name: "Magnus Carlsen",
+          username: "magnus",
+          role: "admin",
+          status: 1
+        },
+      ]
+    }
+  },
+  mounted() {
+  },
+  computed: {
+  },
+  methods: {
+  },
+  watch: {
+  },
 }
 </script>
