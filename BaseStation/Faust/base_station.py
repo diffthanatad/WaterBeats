4db34bc2e806d80d7eea62979a0f2e8b4a61ab2f
Alfiejz <alfiejz@outlook.com>
2023-02-28 18:54:41 +0000

    Faust executable, sensor communication with Faust via web server
    Faust application is now executable directly using Python.
    
    Much easier to launch faust app and run worker now using start_kafka -> start_faust.
    
    Much easier to simulate high volume sensor communication using sensor.py
    
    sensors can now send messages to topics via the exposed local webserver instead of through the command line.
    
    new local storage to track sensor data, sensor messages now only need the bare minimum of sensor id and reading to communicate with Faust.. the rest of the data is automatically filled using the local storage.
    
    fixed local batch storage bug

--- BaseStation/Faust/base_station.py
+++ BaseStation/Faust/base_station.py
@@ -32,20 +32,22 @@ class SensorMessage(faust.Record):
     sensor_type: str = ''
     reading_unit: str = ''
     timestamp: str = ''
+    latitude: float = ''
+    longitude: float = ''
 
 class HumidityMessage(SensorMessage):
-    sensor_type: str = 'humidity'
+    sensor_type: str = 'humidity sensor'
 
-class TemperatureMessage(faust.Record):
-    sensor_type: str = 'temperature'
+class TemperatureMessage(SensorMessage):
+    sensor_type: str = 'temperature sensor'
 
-class SoilMoistureMessage(faust.Record):
-    sensor_type: str = 'soil-moisture'
+class SoilMoistureMessage(SensorMessage):
+    sensor_type: str = 'soil moisture sensor'
 
 # defines humidity, temperature, soil moisture topics to receive sensor messages
 humidity_stream = app.topic('humidity_stream', value_type=HumidityMessage)
 temperature_stream = app.topic('temperature_stream', value_type=TemperatureMessage)
-soil_moisture_stream = app.topic('soil-moisture_stream', value_type=SoilMoistureMessage)
+soil_moisture_stream = app.topic('soil_moisture_stream', value_type=SoilMoistureMessage)
 
 humidity_batch = app.topic('humidity_batch', value_type=HumidityMessage)
 temperature_batch = app.topic('temperature_batch', value_type=TemperatureMessage)
@@ -56,54 +58,51 @@ soil_moisture_batch = app.topic('soil_moisture_batch', value_type=SoilMoistureMe
 @app.agent(humidity_batch)
 async def batch_agent_humidity(batches):
     async for batch in batches.take(MAX_BATCH_SIZE, within=HUMIDITY_BATCH_INTERVAL):
-        # print('batch agent received humidity data')
-        # save to local database
         bp.store_locally(batch)
 
 @app.agent(temperature_batch)
 async def batch_agent_temperature(batches):
     async for batch in batches.take(MAX_BATCH_SIZE, within=TEMPERATURE_BATCH_INTERVAL):
-        # print('batch agent received temperature data')
-        # save to local database
         bp.store_locally(batch)
 
 @app.agent(soil_moisture_batch)
 async def batch_agent_soil_moisture(batches):
     async for batch in batches.take(MAX_BATCH_SIZE, within=SOIL_MOISTURE_BATCH_INTERVAL):
-        # print('batch agent received soil moisture data')
-        # save to local database
         bp.store_locally(batch)
 
+
+
+def fillMessage(message):
+    message.timestamp = get_timestamp()
+    sensor_data = dc.getSensorData(message.sensor_id)
+    message.reading_unit = sensor_data.reading_unit
+    message.latitude = sensor_data.latitude
+    message.longitude = sensor_data.longitude
+    return message
 
 # streaming agents
 @app.agent(humidity_stream, sink=[humidity_batch])
 async def stream_agent_humidity(messages):
     async for message in messages:
-        message.timestamp = get_timestamp()
-        # print('stream agent received humidity data at', message.timestamp)
+        message = fillMessage(message)
         yield message
         #sp.calculateActuatorInstruction(message)
 
 @app.agent(temperature_stream, sink=[temperature_batch])
 async def stream_agent_temperature(messages):
     async for message in messages:
-        message.timestamp = get_timestamp()
-        # print('stream agent received temperature data at', raw_data.timestamp)
+        message = fillMessage(message)
         yield message
         #sp.calculateActuatorInstruction(message)
 
 @app.agent(soil_moisture_stream, sink=[soil_moisture_batch])
 async def stream_agent_soil_moisture(messages):
     async for message in messages:
-        message.timestamp = get_timestamp()
-        # print('stream agent received soil moisture data at', raw_data.timestamp)
+        message = fillMessage(message)
         yield message
         #await sp.calculateActuatorInstruction(message)
 
 
-# @app.timer(interval=2)
-# async def every_2_seconds():
-#     randTemp = random.randint(5, 25)
-#     message = SoilMoistureMessage('someSensorID', randTemp)
-#     print(message)
-#     await soil_moisture_readings.send(value=message)
+
+if __name__ == '__main__':
+    app.main()
