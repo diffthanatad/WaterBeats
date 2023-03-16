f7740aa019f5c39bc1f94bc8c4d49333999e24b8
Alfiejz <alfiejz@outlook.com>
2023-03-16 18:51:42 +0000

    added more tests for rule_engine: apply_rules, load_rule, store_rule, check_task_schedule

--- base_station/rule_engine.py
+++ base_station/rule_engine.py
@@ -26,16 +26,16 @@ def check_relative_condition(relation, sensor_reading, condition_reading):
 def apply_rules(sensor_message):
     tasks = []
     for ruleID in sensor_rules.get(sensor_message.sensor_id, {}):
+        latest_sensor_readings[sensor_message.sensor_id] = sensor_message.reading
         # if rule has a time condition, update the latest sensor reading
         # leave decision making to the event scheduling component
-        if not rules[ruleID].time_condition_message == None:
-            latest_sensor_readings[sensor_message.sensor_id] = sensor_message.reading
-        # else rule does not have a time condition, check if sensor condition is fulfilled
-        else:
-            task = rules[ruleID].task_message
-            condition = rules[ruleID].sensor_condition_message
-            if check_relative_condition(condition.relation, sensor_message.reading, condition.reading):
-                tasks.append(task)
+        if rules[ruleID].time_condition_message:
+            continue
+        # otherwise check if sensor condition is fulfilled
+        task = rules[ruleID].task_message
+        condition = rules[ruleID].sensor_condition_message
+        if check_relative_condition(condition.relation, sensor_message.reading, condition.reading):
+            tasks.append(task)
 
     return tasks
 
@@ -43,7 +43,7 @@ def apply_rules(sensor_message):
 def load_rule(rule_message):
     global ruleID
     rules[ruleID] = rule_message
-    if not rule_message.sensor_condition_message == None:
+    if rule_message.sensor_condition_message:
         sensor_id = rule_message.sensor_condition_message.sensor_subject
         if sensor_id in sensor_rules:
             sensor_rules[sensor_id].add(ruleID)
@@ -57,17 +57,17 @@ def store_rule(rule_message):
         file_location = script_location / 'local_data/rules.txt'
         with open(file_location, 'a', newline='', encoding='utf8') as f:
             f.write(json.dumps(rule_message.task_message.toDict()) + '\t')
-            if rule_message.sensor_condition_message == None:
-                f.write(json.dumps({'msg': 'empty'}) + '\t')
-            else:
+            if rule_message.sensor_condition_message:
                 f.write(json.dumps(rule_message.sensor_condition_message.toDict()) + '\t')
-            if rule_message.time_condition_message == None:
-                f.write(json.dumps({'msg': 'empty'}) + '\n')
             else:
+                f.write(json.dumps({'msg': 'empty'}) + '\t')
+            if rule_message.time_condition_message:
                 f.write(json.dumps(rule_message.time_condition_message.toDict()) + '\n')
+            else:
+                f.write(json.dumps({'msg': 'empty'}) + '\n')
 
     except Exception as e:
-        print(e)
+        print('rule storage no valid file path', e)
 
 
 # apply scheduler
@@ -80,7 +80,7 @@ def check_task_schedule(time_message):
         if not time_condition:
             continue
         sensor_condition_message = rule.sensor_condition_message
-        if not sensor_condition_message == None:
+        if sensor_condition_message:
             relation = sensor_condition_message.relation
             sensor_reading = latest_sensor_readings.get(sensor_condition_message.sensor_subject)
             if sensor_reading == None:
