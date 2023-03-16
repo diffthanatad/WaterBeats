8592948c3276b8d540e49855ccacb0b87823e1b8
Xibo Wang <18601271489@163.com>
2023-03-16 14:24:48 +0000

    actuator part integrate with db part

--- base_station/interfaces/actuator_interface.py
+++ base_station/interfaces/actuator_interface.py
@@ -1,6 +1,10 @@
 import asyncio
+import time
+
+import aiohttp
 import websockets
 import json
+from base_station import device_controller as dc
 
 clients = dict()
 
@@ -14,6 +18,7 @@ async def handle_message(websocket, path):
             if data["type"] == "actuator":
                 clients[data["actuatorId"]] = websocket
                 print("Receive Actuator message: {}".format(json_message))
+                await sendToDB(fill_db_message(data))
             elif data["type"] == "instruction":
                 target = data["target"]
                 command = data["command"]
@@ -21,6 +26,8 @@ async def handle_message(websocket, path):
                 await sendCommand(target, command)
             else:
                 print("Received Message: {}".format(data))
+                await sendToDB(fill_db_message(data))
+
     except Exception as e:
         print("Connection Closed with exception: {}".format(e))
 
@@ -33,10 +40,29 @@ async def sendCommand(actuatorId, command):
     data = json.dumps({"instruction":command})
     await websocket.send(data)
 
+def fill_db_message(data):
+    actuator = dc.get_device(data["actuatorId"])
+    message = {
+        'actuator_id' : data["actuatorId"],
+        'actuator_type' : actuator.actuator_type,
+        'status' : data["status"],
+        'longitude' : actuator.longitude,
+        'latitude' : actuator.latitude,
+        'timestamp' : time.time_ns()
+    }
+    return json.dumps(message)
+
+
+async def sendToDB(message):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post('http://localhost:5555/actuator_data', json=message) as resp:
+                print("Response Status: {}".format(resp.status))
+    except aiohttp.ClientConnectorError as e:
+        print(f"connection is not available: {e}")
+
 if __name__ == '__main__':
     start_server = websockets.serve(handle_message, "0.0.0.0", 8765)
-    # start_server = websockets.serve(server.handle_message, "localhost", 8765)
-
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
 
