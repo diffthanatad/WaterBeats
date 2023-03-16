4b8d35af7b964f1615736ce814441ebf419ca7ae
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-03-16 18:21:00 +0000

    :rocket: :zap: Prepare for Rule Service deployment.

--- AuthGateway/src/controller/proxy.controller.js
+++ AuthGateway/src/controller/proxy.controller.js
@@ -1,11 +1,18 @@
 const proxyService = require('../service/proxy.service');
 
-function proxyRequest(req, res) {
-    proxyService.proxy(req, res, (error) => {
+function proxyRequestBackEnd(req, res) {
+    proxyService.proxyBackEnd(req, res, (error) => {
         res.status(500).send({ message: "Error proxying request: " + error.message });
     });
 }
 
+function proxyRequestRuleService(req, res) {
+    proxyService.proxyServiceRuleService(req, res, (error) => {
+        res.status(500).send({ message: "Error proxying request from Rule Service: " + error.message });
+    });
+}
+
 module.exports = {
-    proxyRequest,
+    proxyRequestBackEnd,
+    proxyRequestRuleService,
 }
--- AuthGateway/src/route/proxy.route.js
+++ AuthGateway/src/route/proxy.route.js
@@ -1,13 +1,20 @@
 module.exports = app => {
     const router = require("express").Router();
     const { authJwt } = require("../middleware");
-    const { proxyRequest } = require("../controller/proxy.controller");
+    const proxyRequest = require("../controller/proxy.controller");
+
+    router.all("/rule-service/*",
+        [
+            authJwt.verifyToken
+        ],
+        proxyRequest.proxyRequestRuleService
+    );
 
     router.all("/*",
         [
             authJwt.verifyToken
         ],
-        proxyRequest
+        proxyRequest.proxyRequestBackEnd
     );
 
     app.use('/', router)
